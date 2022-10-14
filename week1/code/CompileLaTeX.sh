2bf11ff62ea3d33b44c8bad2cc59b1a0caf2275a
Zitong Zhao <zitong.zhao22@imperial.ac.uk>
2022-10-14 13:55:01 +0100

    LaTeX part

---
+++ week1/code/CompileLaTeX.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+x=$(rev <<< $1 | cut -d . -f2- | rev)
+pdflatex $x.tex
+bibtex $x
+pdflatex $x.tex
+pdflatex $x.tex
+evince $x.pdf &
+
+## Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
