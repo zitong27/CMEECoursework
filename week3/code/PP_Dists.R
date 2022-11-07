f79d2fdbc49a979ec3e475fe9b64c2e1a8f714b6
Zitong Zhao <zitong.zhao22@imperial.ac.uk>
2022-11-07 13:59:00 +0000

    assignment submission

--- week3/code/PP_Dists.R
+++ week3/code/PP_Dists.R
@@ -9,41 +9,74 @@ Ratio <- MyData$Prey.mass/MyData$Predator.mass
 logRatio <- log(Ratio)
 feeding <- MyData$Type.of.feeding.interaction
 TempData = data.frame(feeding, logPred, logPrey, logRatio)
+UniqFeeding <- unique(feeding)
+par(mfrow=c(2,3))
+MeanPred<-c()
+MeanPrey<-c()
+MeanRatio<-c()
+MedPred<-c()
+MedPrey<-c()
+MedRatio<-c()
+
 
 pdf("../Results/Pred_Subplots.pdf")
-ggplot(TempData,aes(x=logPred), 
-     xlab="Body Mass (g)", ylab="Count", 
-     main = "Predator mass") +
+ggplot(TempData,aes(x=logPred))+ 
+     labs(x="Body Mass", y="", 
+     title  = "Predator mass") +
   geom_histogram(aes(x=logPred, color = feeding, fill = feeding),
-                     alpha = 0.4,
-                     bins =30)+
-  scale_fill_manual(values = c(2,3,4,5,6))
-
+                 alpha = 0.4,
+                bins =30)+
+  scale_fill_manual(values = c(2,3,4,5,6))+
+  scale_color_manual(values = c(2,3,4,5,6))+
+  facet_wrap( .~ feeding, scales = "free")
+k <- 0
+for (i in UniqFeeding){
+  hist(TempData$logPred[TempData$feeding==i],xlab = i)
+  k <- k+ 1
+  MeanPred[k] <-(mean(TempData$logPred[TempData$feeding==i]))
+  MedPred[k] <-(median(TempData$logPred[TempData$feeding==i]))
+}
 graphics.off();
 
 
 pdf("../Results/Prey_Subplots.pdf")
-ggplot(TempData,aes(x=logPrey), 
-       xlab="Body Mass (g)", ylab="Count", 
-       main = "Prey mass") +
+ggplot(TempData,aes(x=logPrey))+ 
+       labs(x="Body Mass", y="", 
+       title = "Prey mass") +
   geom_histogram(aes(x=logPrey, color = feeding, fill = feeding),
                  alpha = 0.4,
                  bins =30)+
-  scale_fill_manual(values = c(2,3,4,5,6))
-
+  scale_fill_manual(values = c(2,3,4,5,6))+
+  scale_color_manual(values = c(2,3,4,5,6))+
+  facet_wrap( .~ feeding, scales = "free")
+k <- 0
+for (i in UniqFeeding){
+  hist(TempData$logPrey[TempData$feeding==i],xlab = i)
+  k <- k+ 1
+  MeanPrey[k] <-(mean(TempData$logPrey[TempData$feeding==i]))
+  MedPrey[k] <-(median(TempData$logPrey[TempData$feeding==i]))
+}
 graphics.off()
 
 pdf("../Results/SizeRatio_Subplots.pdf")
-ggplot(TempData,aes(x=logratio), 
-       xlab="Body Mass (g)", ylab="Count", 
-       main = "Size Ratio of prey mass and predator mass") +
+ggplot(TempData,aes(x=logratio)) + 
+       labs(x= "Body Mass (g)", y="", 
+       title  = "Size Ratio of prey mass and predator mass") +
   geom_histogram(aes(x=logRatio, color = feeding, fill = feeding),
                  alpha = 0.4,
                  bins =30)+
-  scale_fill_manual(values = c(2,3,4,5,6))
-
+  scale_fill_manual(values = c(2,3,4,5,6))+
+  scale_color_manual(values = c(2,3,4,5,6))+
+  facet_wrap( .~ feeding, scales = "free")
+k <- 0
+for (i in UniqFeeding){
+  hist(TempData$logRatio[TempData$feeding==i],xlab = i)
+  k <- k + 1
+  MeanRatio[k] <-(mean(TempData$logRatio[TempData$feeding==i]))
+  MedRatio[k] <-(median(TempData$logRatio[TempData$feeding==i]))
+}
 graphics.off()
 
-
-Data <- data.frame(logPred, logPrey, Ratio, logRatio)
+Data <- data.frame(MeanPred,MedPred,MeanPrey,MedPrey,MeanRatio,MedRatio)
+rownames(Data) <- UniqFeeding
 write.csv(Data, file = "../Results/PP_Results.csv")
--- week3/code/PP_Regress.R
+++ week3/code/PP_Regress.R
@@ -1,6 +1,26 @@
 rm(list = ls())
-MyDF <- read.csv("../data/EcolArchives-E089-51-D1.csv")
-head(MyDF)
+data <- read.csv("../data/EcolArchives-E089-51-D1.csv")
+head(data)
 
 data$Type.of.feeding.interaction<-as.factor(data$Type.of.feeding.interaction)
 data$Predator.lifestage<-as.factor(data$Predator.lifestage)
+str(data)
+pdf("../results/PP_Regress.pdf")
+p <- ggplot(data, aes(log(Prey.mass),log(Predator.mass)))+
+  geom_point(mapping= aes(color = data$Predator.lifestage), size=I(1), shape=I(3),alpha=0.5)+
+  facet_grid(Type.of.feeding.interaction ~.,scales = "free")+
+  stat_smooth(mapping= aes(color = data$Predator.lifestage,size=I(0.5)), method = "lm", fullrange = TRUE)
+p <- p + theme_bw() + # make the background white
+  theme(aspect.ratio=0.5, legend.position = "bottom",
+        legend.key.size = unit(0.5,"cm"),
+        axis.title.x = element_text(vjust = 1, size = 10),
+        axis.title.y = element_text( size = 10),
+        legend.title = element_text(face='bold',size = 8),
+        legend.text = element_text(size = 8))+
+        guides(color = guide_legend(nrow = 1))+
+  labs(x= "Prey Mass(g)", y = "Predator Mass(g)",
+       color ="Predator.lifestage",size=0.1)
+p
+graphics.off()
+
+summary(data$Prey.mass,data$Predator.mass)
